a1e3bd81ae6a6a9f956311a010f923b8fc6253da
yive77 <dylan.dannenhauer@ignitioncommerce.com>
2019-11-23 21:07:19 -0500

    Added Login and Oauth Components

---
+++ client/src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="">
+        <div>
+            Please Login
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    
+</style>
---
+++ client/src/components/OAuthHandler.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <span>Please Wait...</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'OAuthHandler'
+    }
+</script>
