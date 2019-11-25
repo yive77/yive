a76c59c14370b64e49b040a300472606b4a9bc81
yive77 <dylan.dannenhauer@ignitioncommerce.com>
2019-11-25 18:15:00 -0500

    Implemented store for dependency handling, added header and footer

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="ui inverted vertical footer segment">
+        <div class="ui container">
+            Travel Match 2015. All Rights Reserved
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ client/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="ui secondary pointing menu">
+        <a href="/" class="active item">Home</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style>
+
+</style>
