b4fc8c33f95cc1da0b9db6481ff7389c9398e17d
yive77 <dylan.dannenhauer@ignitioncommerce.com>
2019-11-27 17:12:21 -0500

    Now storing users on register with encrypted passwords and salts

--- server/mongo_handler.py
+++ server/mongo_handler.py
@@ -1,5 +1,7 @@
 from config import Config
 from pymongo import MongoClient
+import uuid 
+import hmac 
 
 class MongoHandler:
 
@@ -7,6 +9,25 @@ class MongoHandler:
 		self.mongo_client = MongoClient(Config.mongodb_connstr)
 		self.db = self.mongo_client["yive"]
 
-	def store_user(self, username, password):
-		pass
+	def exists(self, key, value):
+		existing_record = self.db.users.find_one({key: value})
+		return True if existing_record else False 
 
+	def store_user(self, register_form):
+		email = register_form["email"]
+		if self.exists('email', email):
+			return False
+		user = self.create_user(register_form)
+		self.db.users.insert_one(user)
+
+	def create_user(self, register_form):
+		user = {}
+		user["last_name"] = register_form.get("last_name")
+		user["first_name"] = register_form.get("first_name")
+		user["email"] = register_form.get("email")
+		user["password_hash"], user["salt"] = self.encrypt_user_pw(register_form["password"])
+		
+	def encrypt_user_pw(self, password):
+		user_salt = uuid.uuid4().hex 
+		user_password_hash = hmac.new(b'%s' % user_salt, b'%s' % password).hexdigest()
+		return user_password_hash, user_salt
